28b3c4183d6a50241f72faee789215356083be19
user <user@users-MacBook-Pro.local>
2025-10-11 08:00:08 +0330

    addint alt to vision image in persion

--- frontend/src/app/components/vision/VisionImage.tsx
+++ frontend/src/app/components/vision/VisionImage.tsx
@@ -7,7 +7,7 @@ export default function VisionImage({ className }: { className?: string }) {
     >
       <Image
         src="/images/main_page/vision/education-bro.svg"
-        alt="Conceptual illustration of education and vision in data engineering and artificial intelligence"
+        alt="تصویر مفهومی از آموزش، رشد حرفه‌ای و چشم‌انداز هوش مصنوعی و مهندسی داده در هوبوک"
         width={1500}
         height={1500}
         className="object-contain w-full h-auto"
